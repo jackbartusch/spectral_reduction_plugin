337af0bad8f1aa4c5d42924a820fd9da4be45055
jackbartusch <jackbartusch@gmail.com>
2026-05-06 12:14:45 +1000

    Add files via upload

---
+++ JuceLibraryCode/BinaryData.h
@@ -0,0 +1,60 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   testNub_png;
+    const int            testNub_pngSize = 20876;
+
+    extern const char*   Sugar_Fruit_otf;
+    const int            Sugar_Fruit_otfSize = 45628;
+
+    extern const char*   backgroundFinal_png;
+    const int            backgroundFinal_pngSize = 272269;
+
+    extern const char*   backgroundTest_png;
+    const int            backgroundTest_pngSize = 247024;
+
+    extern const char*   knob1_png;
+    const int            knob1_pngSize = 11638;
+
+    extern const char*   newBackLine_png;
+    const int            newBackLine_pngSize = 11394;
+
+    extern const char*   newSlider_png;
+    const int            newSlider_pngSize = 11445;
+
+    extern const char*   sliderBack_png;
+    const int            sliderBack_pngSize = 5628;
+
+    extern const char*   sliderBackFinal_png;
+    const int            sliderBackFinal_pngSize = 8370;
+
+    extern const char*   testThumb_png;
+    const int            testThumb_pngSize = 2738;
+
+    extern const char*   thumbFinal_png;
+    const int            thumbFinal_pngSize = 13813;
+
+    // Number of elements in the namedResourceList and originalFileNames arrays.
+    const int namedResourceListSize = 11;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Points to the start of a list of resource filenames.
+    extern const char* originalFilenames[];
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes);
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding original, non-mangled filename (or a null pointer if the name isn't found).
+    const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+}
